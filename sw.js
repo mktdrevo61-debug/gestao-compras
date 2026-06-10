9df8ca7176afe4e8e95775f1f67eed8b02665b29
Drevo MKT <mkt@drevomoveis.com>
2026-06-10 17:26:42 -0300

    fix: bump service worker cache version to force update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js - Service Worker simples para PWA Drevo Gestão de Compras
-const CACHE_NAME = 'drevo-compras-v1';
+const CACHE_NAME = 'drevo-compras-v2';
 const ASSETS = [
   './',
   './index.html',
